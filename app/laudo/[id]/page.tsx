3a31b25f66df6ef84d49dfb5d969f829ba2e45ee
Leo Ayres <leoayres@gmail.com>
2026-09-10 22:45:50 -0300

    Force dynamic rendering for LaudoPublico component

--- app/laudo/[id]/page.tsx
+++ app/laudo/[id]/page.tsx
@@ -3,6 +3,8 @@
 import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
 
+export const dynamic = 'force-dynamic';
+
 export default function LaudoPublico() {
   const params = useParams();
   const laudoId = params.id as string;
